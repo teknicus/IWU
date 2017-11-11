22e32dd979f64dc6c906ea609df21d739da4d34b
Robin Biju Thomas <robinbiju4710@gmail.com>
2017-11-11 16:44:16 +0530

    Updated Readme

--- I.W.U_v0.1/I.W.U_v0.1.ino
+++ I.W.U_v0.1/I.W.U_v0.1.ino
@@ -1,23 +1,3 @@
-/* v0.1
-
-Hardware Elements:
-  * Relay Board - 1
-  * LED Strip
-  * RFID Reader
-
-Code Functions:
-  * Read EEPROM and write values to relays
-  * Rising Interrupt on buzzer pin to read RFID Tag
-  * Publish RFID Tag data as string over MQTT
-  * Wait for authentication status
-  * If auth. successful, LED strip to be set to green, red if failed and shuffle between random colours when idle
-  * Listen for MQTT triggers to switch lights and fans
-  * Return Status of each relay when asked
-
-
-  Code By - Robin Biju Thomas
-*/
-
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiClient.h>
