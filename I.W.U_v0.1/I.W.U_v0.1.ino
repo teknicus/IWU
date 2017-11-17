3cb64f277415a7b45cc3b460fbdcf92817623797
Robin Biju Thomas <robinbiju4710@gmail.com>
2017-11-17 11:32:24 +0530

    Added RGB Cycling code
    Cycles through full colour spectrum

--- I.W.U_v0.1/I.W.U_v0.1.ino
+++ I.W.U_v0.1/I.W.U_v0.1.ino
@@ -1,19 +1,58 @@
+
+//***************** Configuration****************************************
+#define tagSize 12
+
+#define rPin D1
+#define gPin D2
+#define bPin D5
+
+
+//***********************************************************************
+
+
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiClient.h>
 #include <EEPROM.h>
 #include "PubSubClient.h"
 
-#define tagSize 12
-
 unsigned int codeLen;
 String buffr;
 char tagCode[tagSize];
 
-void readrfid(){
+uint8_t redPin = rPin;
+uint8_t greenPin = gPin;
+uint8_t bluePin = bPin;
 
-    buffr = Serial.readString();
-    buffr.toCharArray(tagCode,codeLen);
+
+void readrfid() {
+
+  buffr = Serial.readString();
+  buffr.toCharArray(tagCode, codeLen);
+}
+
+void rgbCycle() {
+
+
+  for (int r = 0; r < 1023; r += 5)
+    for (int g = 0; g < 1023; g += 5)
+      for (int b = 0; b < 1023; b += 5) {
+
+        analogWrite(redPin, r);
+        analogWrite(bluePin, b);
+        analogWrite(greenPin, g);
+        delayMicroseconds(1);
+      }
+      
+  for (int r = 1023; r >= 0; r -= 5)
+    for (int g = 1023; g >= 0; g -= 5)
+      for (int b = 1023; b >= 0; b -= 5) {
+
+        analogWrite(redPin, r);
+        analogWrite(bluePin, b);
+        analogWrite(greenPin, g);
+        delayMicroseconds(1);
+      }
 }
 
 void setup() {
@@ -22,6 +61,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+ rgbCycle();
 }
