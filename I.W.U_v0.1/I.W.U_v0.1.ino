262b5d23b5782eee29f46f6b76408651727b5ce7
Robin Biju Thomas <robinbiju4710@gmail.com>
2017-11-17 08:05:23 +0530

    RFID Read function

--- I.W.U_v0.1/I.W.U_v0.1.ino
+++ I.W.U_v0.1/I.W.U_v0.1.ino
@@ -2,8 +2,19 @@
 #include <ESP8266mDNS.h>
 #include <WiFiClient.h>
 #include <EEPROM.h>
-#include <PubSubClient.h>
+#include "PubSubClient.h"
 
+#define tagSize 12
+
+unsigned int codeLen;
+String buffr;
+char tagCode[tagSize];
+
+void readrfid(){
+
+    buffr = Serial.readString();
+    buffr.toCharArray(tagCode,codeLen);
+}
 
 void setup() {
   // put your setup code here, to run once:
